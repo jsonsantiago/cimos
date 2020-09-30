633047594dce8e5c985d54c0b229a8993fd1486c
Ronalyn Anne Pabololot <ronalynannepabololot@gmail.com>
2020-09-30 16:06:37 +0800

    -Updated the page title

--- application/views/cimos/cimos_mobile.php
+++ application/views/cimos/cimos_mobile.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Softplay - Booking</title>
+	<title>Cimos- Covid Declaration</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="<?php echo ASSETS_URL; ?>font/iconsmind/style.css" />
