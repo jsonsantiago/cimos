473968da7660cc319100c3c2056d62db82e79cb9
Ronalyn Anne Pabololot <ronalynannepabololot@gmail.com>
2020-10-01 15:36:14 +0800

    -Updated query

--- application/controllers/Covid.php
+++ application/controllers/Covid.php
@@ -16,7 +16,7 @@ class Covid extends CI_Controller {
 		$data['user_id'] = $_GET['user_id'];
 
 		$data['lead_details'] = $this->Covid_model->get_lead_details($_GET['user_id']);
-
+		
 		$this->load->view('covid/cimos_mobile.php', $data);
 	}
 
